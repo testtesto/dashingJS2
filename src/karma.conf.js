1cb8839c50ce5a0b1e91014f15a2532bedfeeb82
testtesto <benmada96@hotmail.com>
2018-07-17 18:56:53 +0100

    feat(ep444): code coverage
    - Add coverage

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,7 +1,7 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-module.exports = function (config) {
+module.exports = function(config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
@@ -27,10 +27,10 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome'],
     customLaunchers: {
-        ChromeNoSandbox: {
-            base: 'Chrome',
-            flags: ['--no-sandbox']
-        }
+      ChromeNoSandbox: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
     },
     singleRun: false
   });
